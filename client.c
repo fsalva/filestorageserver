6f728688869522197db4e26ba26a6968414575d8
Francesco <francescosalvatori99@gmail.com>
2021-10-23 23:19:45 +0200

    ancora parsing richieste

--- client.c
+++ client.c
@@ -12,6 +12,8 @@
 #include <signal.h>
 #include <pthread.h>
 #include <limits.h>
+#include "./lib/fsstring.h"
+#include "./lib/constvalues.h"
 
 #define BUF_SIZE 4096
 
@@ -19,8 +21,8 @@ int openConnection(const char * sockname, int msec, const struct timespec abst);
 int closeConnection(const char * sockname);
 void print_usage();
 
-void send_request(char *, char);
-char * format_request(char*, int);
+void send_request(int, char, char**);
+char * format_request(char*, int, int);
 char** str_split(char* a_str, const char a_delim);
 
 
@@ -34,10 +36,13 @@ int main(int argc, char const *argv[])
     char * req  = "/tmp/TESTFILES/lipsum15.txt\n";
     char * req2 = "/tmp/TESTFILES/lipsum1.txt\n";
     char * avalue = NULL;
-    char ** arguments;
     
     char *socket_n; //-- path alla server socket
-    
+    char ** arguments;
+    int opcode;
+
+    int c_pid = getpid();
+
     nsecs = 0;
     tfnd = 0;
     flags = 0;
@@ -53,15 +58,9 @@ int main(int argc, char const *argv[])
             socket_n = optarg;
             break;
 
-        case 'R':
-            //strcpy(avalue, optarg);
-            fprintf(stderr, optarg);
+        case 'r':
             arguments = str_split(optarg, (char) ',');
-
-            //avalue = format_request(optarg, opt);
-            //openConnection(NULL, 0, x);
-            //send_request(avalue, opt);
-
+            opcode = OP_READ_FILE;
             break;
         
         case 'h':
@@ -80,23 +79,9 @@ int main(int argc, char const *argv[])
 
     openConnection(socket_n, 100, x);
 
-    if(arguments){
-        int i;
-        
-        for (i = 0; *(arguments + i); i++)
-        {
-            printf("\nfiles=[%s]\n", *(arguments + i));
+    send_request(c_pid, opcode, arguments);
 
-            strcat(*(arguments + i), "\n");
-
-            send_request(*(arguments + i), opt);
-
-            free(*(arguments + i));
-        }
-
-        printf("\n");
-        free(arguments);
-    }
+    
 
     
     //closeConnection();    
@@ -124,7 +109,7 @@ int openConnection(const char * sockname, int msec, const struct timespec abst){
     if(connect(fd_skt, (struct sockaddr *) &sa, sizeof(sa)) == -1){
             perror("[-] Errore in connessione: ");
             exit(EXIT_FAILURE);
-    }   else fprintf(stderr, "\n[+] Connesso.");
+    }   else fprintf(stderr, "[+] Connesso.\n");
     
     return 0;
 }
@@ -134,55 +119,88 @@ int closeConnection(const char * sockname){
     return 0;
 }
 
-char * format_request(char* request_body, int opt){
-
-
+char * format_request(char* request_body, int opt, int pid){
 
     char * fn = (char *) malloc(sizeof(char) * 255);
-    char str[2] = {opt, '\0'};
+    char op_str[sizeof(int) + 2];
+    sprintf(op_str, "%d", opt);
+    
+    fprintf(stderr, op_str);
 
-    strcpy(fn, str);
+    char pid_str[sizeof(int) + 2];
+    sprintf(pid_str, "%d", pid);
+
+    /**
+     * [OPCODE]#[pid]#[request body] 
+     * 
+     * Segue ^ questo modello per formattare le richieste. 
+    */
+    strcpy(fn, op_str);
+    strcat(fn, "#");
+
+    strcat(fn, pid_str);
     strcat(fn, "#");
 
     strcat(fn, request_body);
-    strcat(fn, "#");
+    strcat(fn, "#\n");
 
+    
 
     fprintf(stderr, fn);
     
     return fn;
 }
 
-void send_request(char * request_body, char opt){
+void send_request(int pid, char opt, char ** arguments){
     
-    int letti = 0;
+    if(arguments){
+        int i;
+        char * request = NULL;
 
-    char buf[BUF_SIZE];
-    
-    memset(buf, 0, BUF_SIZE);
+        for (i = 0; *(arguments + i); i++)
+        {
+            request = format_request(*(arguments + i), opt, pid);
 
-    //manda richiesta
-    write(fd_skt, request_body, strlen(request_body));
+            //Invia la richiesta. 
+             int letti = 0;
 
-    size_t bytes_read;
-    size_t dataLen = 0;
-        
-    while((bytes_read = (recv(fd_skt, buf, sizeof(buf), 0))) >= 0){
-        
-        dataLen += bytes_read;
-        
-        if(dataLen > (BUF_SIZE-1)){
-            letti += dataLen;
-            memset(buf, 0, BUF_SIZE);
-            dataLen = 0;
+            char buf[BUF_SIZE];
             
-        } else if( buf[dataLen] == '\000') break; 
+            memset(buf, 0, BUF_SIZE);
+            
+            //invia sulla socket.
+            write(fd_skt, request, strlen(request));
+
+            size_t bytes_read;
+            size_t dataLen = 0;
+                
+            while((bytes_read = (recv(fd_skt, buf, sizeof(buf), 0))) >= 0){
+
+                dataLen += bytes_read;
+                
+                if(dataLen > (BUF_SIZE-1)){
+                    letti += dataLen;
+                    memset(buf, 0, BUF_SIZE);
+                    dataLen = 0;
+                    
+                } else if( buf[dataLen] == '\000') break; 
+            }
+
+            letti += dataLen;
+
+            printf("[PID: %d][+] RECEIVED: %d bytes.\n",getpid(), letti);
+
+
+            //------------------
+
+            free(*(arguments + i));
+        }
+
+        printf("\n");
+        free(arguments);
     }
 
-    letti += dataLen;
-
-    printf("[PID: %d][+] RECEIVED: %d bytes.\n",getpid(), letti);
-
+   
 }
 
 void print_usage(const char * argv[]){
@@ -208,50 +226,3 @@ void print_usage(const char * argv[]){
        , argv[0]);
 }
 
-char** str_split(char* a_str, const char a_delim)
-{
-    char** result    = 0;
-    size_t count     = 0;
-    char* tmp        = a_str;
-    char* last_comma = 0;
-    char delim[2];
-    delim[0] = a_delim;
-    delim[1] = 0;
-
-    /* Count how many elements will be extracted. */
-    while (*tmp)
-    {
-        if (a_delim == *tmp)
-        {
-            count++;
-            last_comma = tmp;
-        }
-        tmp++;
-    }
-
-    /* Add space for trailing token. */
-    count += last_comma < (a_str + strlen(a_str) - 1);
-
-    /* Add space for terminating null string so caller
-       knows where the list of returned strings ends. */
-    count++;
-
-    result = malloc(sizeof(char*) * count);
-
-    if (result)
-    {
-        size_t idx  = 0;
-        char* token = strtok(a_str, delim);
-
-        while (token)
-        {
-            assert(idx < count);
-            *(result + idx++) = strdup(token);
-            token = strtok(0, delim);
-        }
-        assert(idx == count - 1);
-        *(result + idx) = 0;
-    }
-
-    return result;
-}
